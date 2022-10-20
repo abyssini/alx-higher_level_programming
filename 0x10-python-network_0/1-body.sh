1eb4f6ec757b07a1a0a0db5373bcd85aa68ee1d4
abyssini <abyssiniazelalem874@gmail.com>
2022-10-21 00:52:12 +0300

    python network-0

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL and displays the size of the body of the response
-curl -so /dev/null "$1" -w '%{size_download}\n'
+# takes in a URL, sends a GET request to the URL and displays the body of the response
+curl -sL "$1"
